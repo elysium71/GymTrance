ef7abdefa945f5f61d1f27de9b81c293d8e4dec1
elysium71 <elysium0567@gmail.com>
2026-04-18 18:18:22 +0800

    Added input validation to POST endpoint and save data
    save data into workouts.json

--- app/app.py
+++ app/app.py
@@ -1,16 +1,42 @@
 from flask import Flask, render_template, request, jsonify
+import json
+
 
 app = Flask(__name__)
+
+
+# save data into json file.
+DATA_FILE = "data/workouts.json"
+
+def load_workouts():
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+def save_workouts(workouts):
+    with open(DATA_FILE, "w") as file:
+        json.dump(workouts, file, indent=4)
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+# get the data in json file.
 @app.route('/data', methods=['GET'])
 def get_data():
-    return {"id": 1, "Workout": "Bench Press"}
+    workouts = load_workouts()
+    return jsonify(workouts)
 
+# post the data in json file.
 @app.route('/data', methods=['POST'])
 def handle_post():
     data = request.get_json()
-    return jsonify({"received": data}), 201
+
+    workouts = load_workouts()
+    workouts.append(data)
+    save_workouts(workouts)
+
+    return jsonify({
+        "message": "Workout added successfully",
+        "received": data
+    }), 201
