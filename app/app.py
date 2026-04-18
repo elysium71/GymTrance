e9ed352c367586c3c7aefb684837dc78c2cce58f
elysium71 <elysium0567@gmail.com>
2026-04-18 18:10:43 +0800

    Added API endpoints and tested GET/POST with Thunder Client
    Installed Thunder Client to test POST a workout

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return {"id": 1, "Workout": "Bench Press"}
+
+@app.route('/data', methods=['POST'])
+def handle_post():
+    data = request.get_json()
+    return jsonify({"received": data}), 201
